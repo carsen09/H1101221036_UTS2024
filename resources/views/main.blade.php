d13b130f64b3be81ebdf5c090738397a7f5e7f27
Carsen Wilmer <carsenwilmer9@gmail.com>
2024-03-29 17:52:28 +0700

    update file

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="./assets/img/favicon.png">
-  <!-- <link rel="stylesheet" href="assets/css/argon-dashboard.css"> -->
   <title>
     Dashboard Pegawai
   </title>
